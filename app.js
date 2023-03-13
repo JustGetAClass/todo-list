188beab0a06f39168e83d40198cb585694e19e1f
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-03-13 12:26:39 +0300

    added insert

--- app.js
+++ app.js
@@ -58,15 +58,13 @@ async function main() {
 	});
 
 	app.post("/", function (req, res) {
-		const item = req.body.newItem;
+		const itemName = req.body.newItem;
+		const item = new Item({
+			name: itemName,
+		});
 
-		if (req.body.list === "Work") {
-			workItems.push(item);
-			res.redirect("/work");
-		} else {
-			items.push(item);
-			res.redirect("/");
-		}
+		item.save();
+    res.redirect("/")
 	});
 
 	app.get("/work", function (req, res) {
