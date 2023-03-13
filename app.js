3f6e3e558f51cc48633f1182453596387e684ba8
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-03-13 12:07:41 +0300

    create default items, read items from database

--- app.js
+++ app.js
@@ -1,45 +1,83 @@
 //jshint esversion:6
 
 const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+// const date = require(__dirname + "/date.js");
 
 const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// * adding a database
 
+main().catch((err) => console.log(err));
 
-app.get("/", function (req, res) {
-	const day = date.getDate();
+async function main() {
+	const url = "mongodb://127.0.0.1:27017/todolistDB";
+	await mongoose.connect(url, { useNewUrlParser: true });
 
-	res.render("list", { listTitle: day, newListItems: items });
-});
+	const itemsSchema = new mongoose.Schema({
+		name: String,
+	});
 
-app.post("/", function (req, res) {
-	const item = req.body.newItem;
+	const Item = new mongoose.model("Item", itemsSchema);
 
-	if (req.body.list === "Work") {
-		workItems.push(item);
-		res.redirect("/work");
-	} else {
-		items.push(item);
-		res.redirect("/");
-	}
-});
+	const item1 = new Item({
+		name: "Welcome to your todolist",
+	});
 
-app.get("/work", function (req, res) {
-	res.render("list", { listTitle: "Work List", newListItems: workItems });
-});
+	const item2 = new Item({
+		name: "Hit the + button to add a new item",
+	});
 
-app.get("/about", function (req, res) {
-	res.render("about");
-});
+	const item3 = new Item({
+		name: "<----- Hit this to delete an item",
+	});
 
-app.listen(3000, function () {
-	console.log("Server started on port 3000");
-});
+	const defaultItems = [item1, item2, item3];
+
+	app.get("/", function (req, res) {
+		// const day = date.getDate();
+
+		Item.find()
+			.then((items) => {
+				if (items.length === 0) {
+					Item.insertMany(defaultItems)
+						.then(() =>
+							console.log("Successfully added items to database")
+						)
+						.catch((err) => console.log(err));
+					items = defaultItems;
+				}
+				res.render("list", { listTitle: "Today", newListItems: items });
+			})
+			.catch((err) => console.log(err));
+	});
+
+	app.post("/", function (req, res) {
+		const item = req.body.newItem;
+
+		if (req.body.list === "Work") {
+			workItems.push(item);
+			res.redirect("/work");
+		} else {
+			items.push(item);
+			res.redirect("/");
+		}
+	});
+
+	app.get("/work", function (req, res) {
+		res.render("list", { listTitle: "Work List", newListItems: workItems });
+	});
+
+	app.get("/about", function (req, res) {
+		res.render("about");
+	});
+
+	app.listen(3000, function () {
+		console.log("Server started on port 3000");
+	});
+}
--- date.js
+++ date.js
@@ -1,27 +1,27 @@
 //jshint esversion:6
 
-exports.getDate = function() {
+// exports.getDate = function() {
 
-  const today = new Date();
+//   const today = new Date();
 
-  const options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
+//   const options = {
+//     weekday: "long",
+//     day: "numeric",
+//     month: "long"
+//   };
 
-  return today.toLocaleDateString("en-US", options);
+//   return today.toLocaleDateString("en-US", options);
 
-};
+// };
 
-exports.getDay = function () {
+// exports.getDay = function () {
 
-  const today = new Date();
+//   const today = new Date();
 
-  const options = {
-    weekday: "long"
-  };
+//   const options = {
+//     weekday: "long"
+//   };
 
-  return today.toLocaleDateString("en-US", options);
+//   return today.toLocaleDateString("en-US", options);
 
-};
+// };
